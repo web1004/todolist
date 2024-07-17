9e75a214681684d8f2be2dcfbf814eb72d4f958c
web1004 <gangsugi@gmail.com>
2024-07-17 17:14:53 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import './App.css';
+import { useState, useRef, useReducer } from 'react';
+import Header from './components/Header';
+import Editor from './components/Editor';
+import List from './components/List';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "청소하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "과제하기",
+    date: new Date().getTime(),
+  },
+];
+
+function reducer(state, action){
+  switch(action.type){
+    case 'CREATE': 
+      return [action.data, ...state];
+    case 'UPDATE': 
+      return state.map((item) => 
+        item.id === action.targetId 
+        ? {...item, isDone: !item.isDone }
+        : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => item.id !== action.targetId
+      );
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  //숫자,문자만 넣을수 있다.
+  localStorage.setItem('test',"hello");
+
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  };
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate}/>
+      <List 
+        todos={todos} 
+        onUpdate={onUpdate} 
+        onDelete={onDelete}
+      />
+    </div>
+  );
+}
+
+export default App;
